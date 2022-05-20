fcbc4862f46c24c2916c799e4c48276fdef282db
Dennis Ammen <dennis.ammen@outlook.com>
2022-05-20 17:18:23 +0200

    added loader

--- script.js
+++ script.js
@@ -3,11 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// Show loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide loading
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 // Show new Quote
 function newQuote(quotesArray) {
+    loading();
     // Pick a random quote from Quotes array
     const quote = quotesArray[Math.floor(Math.random() * quotesArray.length)];
     // Check if author field is blank and replace it with 'Unknown'
@@ -22,11 +36,14 @@ function newQuote(quotesArray) {
     } else {
         quoteText.classList.remove('long-quote');
     }
+    // Set quote, hide loader
     quoteText.textContent = quote.text;
+    complete();
 }
 
 // Get quotes from API
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://type.fit/api/quotes'
     try {
         const response = await fetch(apiUrl);
